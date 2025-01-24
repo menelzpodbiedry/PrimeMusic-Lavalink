e7c835497cce0016872b3bf03b60b5b09b3cccc7
menelzpodbiedry <menelzpodbiedry111@gmail.com>
2025-01-24 21:01:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://kandex85:kandex85@cluster0.bnroz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "P&S CLOUD MUSIC | wtf ?", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/tnJXDxZau7",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
