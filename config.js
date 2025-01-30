df28d687ef20aedc8c8f125078173b3836ed81a7
menelzpodbiedry <menelzpodbiedry111@gmail.com>
2025-01-30 17:27:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
     {
-      name: "INZEWORLD.COM (DE)",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
-      secure: false
+     name: "ChalresNaig Node",
+     password: "NAIGLAVA-dash.techbyte.host",
+     host: "lavahatry4.techbyte.host",
+     port: 3000,
+     secure: false
     },
     {
       name: "Catfein ID",
